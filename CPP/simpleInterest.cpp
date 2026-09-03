8b12c656a630272258c70b1f5169f25de5e4242c
Sohan-2807 <thamminenisohan1977@gmail.com>
2026-09-03 11:36:41 +0530

    Added default Arguments

--- CPP/simpleInterest.cpp
+++ CPP/simpleInterest.cpp
@@ -1,13 +1,13 @@
 #include<iostream>
 using namespace std;
 
-int SimpleInterest(int principal,int time,int rate){
+int SimpleInterest(int principal,int time ,int rate = 10){
     cout<<"Simple Interest is:"<<endl;
     int si = principal*time*rate/100;
     return si;
 }
 
 int main(){
-    cout<<SimpleInterest(10000,2,10);
+    cout<<SimpleInterest(10000,2);
 }
 
